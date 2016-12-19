0682e6e20554805125c5c54fdc14ab07788ad279
escalusVI <Loicvaleryvi@gmail.com>
2016-12-19 21:57:17 +0100

    correction fautes ortographes

--- include/sectionHeader.php
+++ include/sectionHeader.php
@@ -2,5 +2,5 @@
 <div class="textarea">
 	<h1>A propos</h1>
 	<p>L'AEBE a pour objet de participer au bien être des enfants de Berville et d'Etalleville.
-	L'association pourra mettre en place diverses manifestations ayant pour but de récolter de récolter de l'argent. Cet argent pourra ensuite être reversé aux différentes coopératives scolaire afin d'aider à la mise en place de sorties scolaire, ou d'achat de matériels.</p>
+	L'association pourra mettre en place diverses manifestations ayant pour but de récolter de récolter de l'argent. Cet argent pourra ensuite être reversé aux différentes coopératives scolaires afin d'aider à la mise en place de sorties scolaires, ou d'achat de matériels.</p>
 	</div>
