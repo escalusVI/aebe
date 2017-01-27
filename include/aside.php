039f42157a1c61514ba4c9c05ef7cca91b8ae4d9
escalusVI <loicvaleryvi@gmail.com>
2017-01-27 13:58:34 +0100

    ajout fichier php manquants

---
+++ include/aside.php
@@ -0,0 +1,28 @@
+<aside class="col-md-2 visible-lg">
+	<section class="col-md-12 visible-lg calendrier">
+		<iframe src="https://calendar.google.com/calendar/embed?title=evenements&amp;showTitle=0&amp;showNav=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;height=300&amp;wkst=2&amp;hl=fr&amp;bgcolor=%2333ccff&amp;src=aebe.asso%40gmail.com&amp;color=%231B887A&amp;ctz=Europe%2FParis" style="border:solid 1px #777" width="400" height="300" frameborder="0" scrolling="no" ></iframe>
+	</section>
+
+	<section class="col-md-12 visible-lg menuCantine">
+		<a href="">
+		<div>
+			<h2>Menu cantine</h2>
+			<h3>lundi</h3>
+			<p>
+				<h4>entrée</h4>
+					betterave<br>
+				
+				<h4>plat de resistance</h4>
+					frites<br>
+					steack haché<br>
+					
+				<h4>dessert</h4>
+					glace
+					
+			</p>
+		</div>
+		</a>		
+		
+		</p>
+	</section>
+</aside>
---
+++ include/crayon.php
@@ -0,0 +1,9 @@
+<div class="col-lg-1 background">
+	<div class="row">
+		<div>
+			<img src="images/hautCrayonGauche.png" class="col-lg-12">
+			<img src="images/milieuCrayonGauche.png" class="col-lg-12">
+			<img src ="images/basCrayonGauche.png" class="col-lg-12">
+		</div>
+	</div>
+</div>
