59fbfda6bd13ec0f5fb5bf24699b96cd21cb3135
escalusVI <loicvaleryvi@gmail.com>
2017-03-01 20:36:54 +0100

    menu cantine aside caché le temps de le rendre operationnel

--- include/aside.php
+++ include/aside.php
@@ -3,7 +3,7 @@
 		<iframe src="https://calendar.google.com/calendar/embed?title=evenements&amp;showTitle=0&amp;showNav=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;height=300&amp;wkst=2&amp;hl=fr&amp;bgcolor=%2333ccff&amp;src=aebe.asso%40gmail.com&amp;color=%231B887A&amp;ctz=Europe%2FParis" style="border:solid 1px #777" width="400" height="300" frameborder="0" scrolling="no" ></iframe>
 	</article>
 
-	<section class="col-md-12 visible-lg menuCantine">
+	<!--<section class="col-md-12 visible-lg menuCantine">
 		<a href="cantine.php">
 		<div>
 			<h2>Menu cantine</h2>
@@ -24,5 +24,5 @@
 		</a>		
 		
 		</p>
-	</section>
+	</section>-->
 </aside>
