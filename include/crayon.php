3d978cafb12175110371a02d6bf65313573232f9
escalusVI <loicvaleryvi@gmail.com>
2017-03-02 13:39:07 +0100

    ajout information footer realisation et hebergeur

--- include/crayon.php
+++
@@ -1,9 +0,0 @@
-<div class="col-lg-1 background">
-	<div class="row">
-		<div>
-			<img src="images/hautCrayonGauche.png" class="col-lg-12">
-			<img src="images/milieuCrayonGauche.png" class="col-lg-12">
-			<img src ="images/basCrayonGauche.png" class="col-lg-12">
-		</div>
-	</div>
-</div>
--- include/footer.php
+++ include/footer.php
@@ -2,7 +2,7 @@
 	<div class="pull-left col-md-2">
 		<a href= "https://www.facebook.com/groups/A.E.B.E76/?fref=ts" target="_blank"><img src="images/facebookBlanc.png" class="icones"></a>
 	</div>
-	<div class="col-md-4 col-md-offset-2 col-xs-7 cgv">Site réalisé par Loic valery<br/>Hébergement par <a href="http://www.aptiko.com/" target="_blank"><img src="images/aptiko.png" class="aptiko"/></a> </div>
+	<div class="col-md-5 col-md-offset-2 col-xs-7 cgv">Site réalisé par Loic valery / Hébergement par <a href="http://www.aptiko.com/" target="_blank"><img src="images/aptiko.png" class="aptiko"/></a> </div>
 	<div class="pull-right col-md-offset-1 col-md-2">
 		<a href= "mailto:aebe.asso@gmail.com"><img src="images/mail.png" class="pull-right icones"></a>
 	</div>
