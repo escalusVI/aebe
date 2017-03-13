ff81a4aaebd90cb578834a234f4b1a4432321956
Loic Valery <loicvaleryvi@gmail.com>
2017-03-13 13:56:59 +0100

    changement footer remplacement aptiko par image in

--- include/footer.php
+++ include/footer.php
@@ -1,9 +1,9 @@
 <footer class="row">	
-	<div class="pull-left col-md-2">
+	<div class="pull-left col-md-2 col-xs-1">
 		<a href= "https://www.facebook.com/groups/A.E.B.E76/?fref=ts" target="_blank"><img src="images/facebookBlanc.png" class="icones"></a>
 	</div>
-	<div class="col-md-5 col-md-offset-2 col-xs-7 cgv">Site réalisé par Loic valery / Hébergement par <a href="http://www.aptiko.com/" target="_blank"><img src="images/aptiko.png" class="aptiko"/></a> </div>
-	<div class="pull-right col-md-offset-1 col-md-2">
+	<div class="col-md-6 col-md-offset-1 col-xs-8 col-xs-offset-1 cgv">Site réalisé par Loic valery / Hébergé par <a href="http://www.image-in.fr/" target="_blank"><img src="images/imagein.png" class="imagein"/></a> </div>
+	<div class="pull-right col-md-offset-1 col-md-2 col-xs-1">
 		<a href= "mailto:aebe.asso@gmail.com"><img src="images/mail.png" class="pull-right icones"></a>
 	</div>
 </footer>
