084752dae698743bd222fc5eefbf712383158465
Loic Valery <loicvaleryvi@gmail.com>
2017-04-15 13:35:14 +0200

    correction problemes affichage menu responsive

--- index.php
+++ index.php
@@ -1,19 +1,24 @@
-<!DOCTYPE HTML>
+<!DOCTYPE html>
 <html>
 	<head>
-		<meta charset="utf-8">
-		<link rel="stylesheet" href="bootstrap/css/bootstrap.css">
-		<link rel = "stylesheet" href= " style.css">
-		<link rel="icon" href="images/logo.png" type="image/png">
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width-device-width, initial-scale=1.0" />
 		<title>AEBE</title>
+		<link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+		<link rel="stylesheet" href="style.css">
+		<link rel="icon" href="images/logo.png" type="image/png">
 	</head>
+	
 	<body>
 
+	
 		<div class="col-md-10 col-md-offset-1 container">
-	
-		<?php include ("include/header.php");?>
-	
-
+		
+		
+		
+			<?php include ("include/header.php");?>
+			
+		
 		<section class="row">
 			<article class="col-md-offset-1 col-md-6">
 				<div class="row textarea">
@@ -25,18 +30,13 @@
 			<?php include ("include/aside.php");?>
 			
 		</section>
-			
-			<!--
-			<div class="textarea">
-				<iframe src="https://calendar.google.com/calendar/embed?title=evenements&amp;showTitle=0&amp;showNav=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;height=300&amp;wkst=2&amp;hl=fr&amp;bgcolor=%2333ccff&amp;src=aebe.asso%40gmail.com&amp;color=%231B887A&amp;ctz=Europe%2FParis" style="border:solid 1px #777" width="400" height="300" frameborder="0" scrolling="no" ></iframe>
-				</br>
-				<a href="content/affiche.png" target="_blanck"><img src="content/affiche.png" alt="affiche du marché de noël" style=" width: 40%"/></a>
-			</div>
-
-		</section>
-		-->
 		
-		<?php include ("include/footer.php");?>
+		
+			<?php include ("include/footer.php");?>
+			
+		
+		</div>
+		<script src="bootstrap/js/bootsrap.js"></script>
+		<script src="bootstrap/js/jquery.js"></script>
 	</body>
-
 </html>
--- member.php
+++ member.php
@@ -1,19 +1,24 @@
-<!DOCTYPE HTML>
+<!DOCTYPE html>
 <html>
 	<head>
-		<meta charset="utf-8">
-		<link rel="stylesheet" href="bootstrap/css/bootstrap.css">
-		<link rel = "stylesheet" href= " style.css">
-		<link rel="icon" href="images/logo.png" type="image/png">
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width-device-width, initial-scale=1.0" />
 		<title>AEBE</title>
+		<link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+		<link rel="stylesheet" href="style.css">
+		<link rel="icon" href="images/logo.png" type="image/png">
 	</head>
+	
 	<body>
 
-		<div class="col-md-10 col-md-offset-1 container">
-
-		<?php include ("include/header.php");?>
-
 	
+		<div class="col-md-10 col-md-offset-1 container">
+		
+		
+		
+			<?php include ("include/header.php");?>
+			
+		
 		<section class="row">
 			<article class="col-md-offset-1 col-md-6">
 				<div class="textarea">
@@ -33,7 +38,11 @@
 		</section>
 		
 		
-		<?php include ("include/footer.php");?>
+			<?php include ("include/footer.php");?>
+			
+		
+		</div>
+		<script src="bootstrap/js/bootsrap.js"></script>
+		<script src="bootstrap/js/jquery.js"></script>
 	</body>
-
 </html>
